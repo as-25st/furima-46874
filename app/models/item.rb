8f3f56c09dd2da0bbfe3a3fc2b33e0070b8289f7
as-25st <shift69lv@yahoo.co.jp>
2026-04-07 23:23:06 +0900

    Itemモデルと関連テーブルを作成

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  has_one_attached :image
+end
