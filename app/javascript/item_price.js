2c318eeb018571c100e2cbbe47400e9d41ec39a2
as-25st <shift69lv@yahoo.co.jp>
2026-04-09 18:11:46 +0900

    販売手数料と販売利益の自動計算機能を実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,17 @@
+function calcPrice (){
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const price = Number(inputValue);
+    const tax = Math.floor(price * 0.1);
+    const profit = Math.floor(price - tax);
+    addTaxDom.textContent = tax.toLocaleString();
+    profitDom.textContent = profit.toLocaleString();
+  });
+};
+
+window.addEventListener('turbo:load', calcPrice);
+
