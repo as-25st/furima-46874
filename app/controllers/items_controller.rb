9419195d7453df68b6409a51633f71c90426dafe
as-25st <shift69lv@yahoo.co.jp>
2026-04-14 23:36:16 +0900

    商品情報の更新機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,12 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
