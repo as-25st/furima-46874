eaeee9a26a317b8067206e5962f3bbb43f7f563c
as-25st <shift69lv@yahoo.co.jp>
2026-04-16 23:19:08 +0900

    商品削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :items
 end
