b2457e0343dc0d0970130a10066a6d61e151d11a
as-25st <shift69lv@yahoo.co.jp>
2026-04-23 18:10:14 +0900

    商品購入機能におけるバリデーションを修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,20 +2,19 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token
 
-  with_options presence: true do
-    validates :user_id
-    validates :item_id
-    validates :postal_code
-    validates :prefecture_id
-    validates :city
-    validates :address
-    validates :phone_number
-    validates :token
-  end
-
+  validates :token, presence: true
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  validates :postal_code, presence: true
   validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+  validates :prefecture_id, presence: true
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :phone_number, length: { minimum: 10, message: 'is too short' }
+  validates :phone_number, length: { maximum: 11, message: 'is too long' }
+  validates :phone_number, format: { with: /\A\d+\z/, message: 'is invalid. Input only number' }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
