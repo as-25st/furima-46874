e3dc90ad531dbf5b83c69c7ab880612c21e0744e
as-25st <shift69lv@yahoo.co.jp>
2026-05-02 23:22:04 +0900

    ログイン状態と購入状態に応じたアクセス制限を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address = OrderAddress.new
@@ -45,5 +47,11 @@ class OrdersController < ApplicationController
       card: order_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def move_to_index
+    return unless @item.order.present? || current_user.id == @item.user_id
+
+    redirect_to root_path
   end
 end
